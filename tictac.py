c5ef1f7956f3dfab882e84eb23e5fc45349b0006
megandls <megandeleon5@gmail.com>
2022-04-20 17:22:19 -0600

    first move done

--- tictac.py
+++ tictac.py
@@ -1,82 +1,90 @@
 """CSE210 W01: A game of Tic Tac Toe"""
 __author__ = "Megan De Leon"
 
-
+"""WELCOME"""
 print("")
-print("\033[3;32;43m Welcome to Tic Tac Toe  \n") 
+print("\033[3;32;43m  Welcome to Tic Tac Toe  \n") 
 
+"""USERNAMES"""
 """Get usernames"""
 username = input("   Enter P1 Username: ")
-print("Hello, " + username + "! You are X's")
+print(" Hello, " + username + "! You are X's")
 print (" ")
 username2 = input("   Enter P2 Username: ")
-print("Hello, " + username2 + "! You are O's")
+print(" Hello, " + username2 + "! You are O's")
 
-
-# name = ""
-# player = ""
-"""Get usernames"""    
-# def user_names(name, player):
-#     print("Hello, " + name + "! You are " + player + "'s")
-#     p1 = "X"
-#     p2 = "O"
-#     users = {}
-#     k = 0
-#     while k < 2:
-#         key = input("What is your name? ")
-#         value = p1
-#         users[key] = value
-#         k += 1
-#         if k == 1:
-#             user_names(key, p1)
-#         elif k == 2:
-#             user_names(key, p2)       
-#             print ("Here is the grid, Good Luck!" + users)
-# print("Hello, " + username + "! You are X's")
-# print (" ")
-# username2 = input("Enter P1 username: ")
-# print("Hello, " + username2 + "! You are O's")
+"""GRID"""
+"""Grid lines defaults"""
+row1 = " 1|2|3"
+row2 = " 4|5|6"
+row3 = " 7|8|9"
 
 """Draw a Grid"""                          
 def grid():
     divider = "-+-+-"
     print (" ")
-    print ("1|2|3")
-    print (divider)
-    print ("4|5|6")
-    print (divider)
-    print ("7|8|9")
+    print(row1)
+    print (" " + divider)
+    print(row2)
+    print (" " + divider)
+    print(row3)
+    turn("X")
 
-# nums = [] #empty list
-# for i in range(3): #start at 0
-#     nums.append([]) #still empty
-#     for j in range(1, 4): # j starts at 1
-#         nums[i].append(j) #back and forth to add 1, 2, and 3 to the original list - go back to beginning of i loop to start at 1 to have three sets
-# print("select a number:")
-# print(nums)
-
-#Or reuse key from dictionary? for usernames.
-
-# """Loop for input 3x"""
-# print (" ")
-# x = 0
-# while x < 9:
-#     """Get input from Player 1"""
-#     move1 = input("X's choose a number: ")
-#     if move1 == 1
-
+"""Get input from Player 1"""
+"Move 1"
+def turn(sign):
+    print(" ")
+    move1 = input(" " + sign + "'s turn to choose a square (1-9): _")
+    if move1 == "1":
+        print(" " + sign + "|2|3")
+        print(row2)
+        print(row3)
+    elif move1 == "2":
+        print("1|" + sign + "|3")
+        print(row2)
+        print(row3)
+    elif move1 == "3":
+        print("1|2|" + sign)
+        print(row2)
+        print(row3)
+    elif move1 == "4":
+        print(row1)
+        print(" " + sign + "|5|6")
+        print(row3)
+    elif move1 == "5":
+        print(row1)
+        print("4|" + sign +"|6")
+        print(row3)
+    elif move1 == "6":
+        print(row1)
+        print("4|5|" + sign)
+        print(row3)
+    elif move1 == "7":
+        print(row1)
+        print(row2)
+        print(" " + sign + "|8|9")
+    elif move1 == "8":
+        print(row1)
+        print(row2)
+        print("7|" + sign + "|9")
+    elif move1 == "9":
+        print(row1)
+        print(row2)
+        print("7|8|" + sign)
+    
 """Get input from Player 2"""
-print ("O's choose a number: ")
+turn("O")
+print("")
 
 def main():
     grid()
     print("")
-    answer = input("Would you like to play again? y/n ")
+    answer = input(" Would you like to play again? y/n ")
     if answer == "y": 
-        print("Yes!") #how to start over?
+        print(" Yes!") #how to start over?
         print("")    
     else:
-        print("Good game. Thanks for playing!")
+        print(" Good game. Thanks for playing!")
         print("")
 
 if __name__ =="__main__":
