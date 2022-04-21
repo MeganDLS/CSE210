e65247ad3732f7a7426b11bbb11158b5de46884a
megandls <megandeleon5@gmail.com>
2022-04-21 10:06:44 -0600

    all good, except the extra list

--- tictac.py
+++ tictac.py
@@ -1,109 +1,73 @@
-"""CSE210 W01: A game of Tic Tac Toe"""
-__author__ = "Megan De Leon"
+"""
+CSE210 W01: 
+A game of Tic Tac Toe
+Megan De Leon
+"""
 
 """WELCOME"""
-print("")
-print("\033[3;32;43m  Welcome to Tic Tac Toe  \n") 
+print("\n \033[3;32;43m  Welcome to Tic Tac Toe  \n") 
 
 """USERNAMES"""
 """Get usernames"""
-username = input("   Enter P1 Username: ")
-print(" Hello, " + username + "! You are X's")
-print (" ")
-username2 = input("   Enter P2 Username: ")
-print(" Hello, " + username2 + "! You are O's")
+def assign_names():
+    pass
 
-"""GRID"""
+
 """Grid lines defaults"""
-row1 = " 1|2|3"
-row2 = " 4|5|6"
-row3 = " 7|8|9"
+def create_board():
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    return board
 
-"""Draw a Grid"""                          
-def grid():
-    divider = "-+-+-"
-    print (" ")
-    print(row1)
-    print (" " + divider)
-    print(row2)
-    print (" " + divider)
-    print(row3)
-#    turn("X")
+def display_board(board):
+    print(f"""
+    {board[0]}|{board[1]}|{board[2]}
+    -+-+-
+    {board[3]}|{board[4]}|{board[5]}
+    -+-+-
+    {board[6]}|{board[7]}|{board[8]}
+    """)
 
-"""Get input from Players"""
-"Move 1"
-def turn(sign):
-    print(" ")
-    move1 = input(" " + sign + "'s turn to choose a square (1-9): _")
-    if move1 == "1":
-        global row1 
-        row1 = (" " + sign + "|2|3")
-        print(row1)
-        print(row2)
-        print(row3)
-    elif move1 == "2":
-        #global row1
-        row1 = ("1|" + sign + "|3")
-        print(row1)
-        print(row2)
-        print(row3)
-    elif move1 == "3":
-        #global row1
-        row1 = ("1|2|" + sign)
-        print (row1)
-        print(row2)
-        print(row3)
-    elif move1 == "4":
-        print(row1)
-        global row2
-        row2 = (" " + sign + "|5|6")
-        print(row2)
-        print(row3)
-    elif move1 == "5":
-        print(row1)
-        #global row2
-        row2 = ("4|" + sign +"|6")
-        print(row2) 
-        print(row3)
-    elif move1 == "6":
-        print(row1)
-        #global row2
-        row2 = ("4|5|" + sign)
-        print(row2)
-        print(row3)
-    elif move1 == "7":
-        print(row1)
-        print(row2)
-        global row3
-        row3 = (" " + sign + "|8|9")
-        print(row3)
-    elif move1 == "8":
-        print(row1)
-        print(row2)
-        #global row3
-        row3 = ("7|" + sign + "|9")
-        print(row3)
-    elif move1 == "9":
-        print(row1)
-        print(row2)
-        #global row3
-        row3 = ("7|8|" + sign)
-        print(row3)
-    
-print("")
+def edit_board(board, sign):
+    square = int(input(sign + " turn to choose a square (1-9): "))
+    board[square - 1] = sign
+    print(board)    
+
+def change_sign(sign):
+    sign = "X" if sign == "O" else "O" 
+    print("Winner, Winner!")
+    return sign
+
+def is_game_over(board):
+    if (board[0] == board[1] == board[2]
+    or board[3] == board[4] == board[5]
+    or board[6] == board[7] == board[8]
+    or board[0] == board[3] == board[6]
+    or board[1] == board[4] == board[7]
+    or board[2] == board[5] == board[8]
+    or board[0] == board[4] == board[8]
+    or board[6] == board[4] == board[2]):
+        return True
+    return False
+
 
 def main():
-    grid()
-    t = 0
-    while t < 3:
-        turn("X")
-        turn("O")
-        t += 1
-    
-    print("")
-    answer = input(" Would you like to play again? y/n ")
+    username = input("   Enter P1 Username: ")
+    print(" Hello, " + username + "! You are X's")
+    print (" ")
+    username2 = input("   Enter P2 Username: ")
+    print(" Hello, " + username2 + "! You are O's")
+    sign = "X"
+    board = create_board()
+   
+    while not is_game_over(board):
+        display_board(board)
+        edit_board(board, sign)
+        sign = change_sign(sign)
+
+    answer = input("\n Would you like to play again? y/n ")
     if answer == "y": 
-        print(" Yes!") #how to start over?
+        print(" Yes!")
+        main()
         print("")    
     else:
         print(" Good game. Thanks for playing!")
